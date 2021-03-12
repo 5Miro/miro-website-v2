aaaed53bdb76b0ee2e25cb0ef615761b1b763beb
Miro <43798115+5Miro@users.noreply.github.com>
2021-03-12 17:54:46 -0300

    V2.1.2
    - bugfix: /seed is no longer available from the api
    - emarket now has thumbnail
    - watch btn animation is shorter on the way back

--- backend/routers/projectRouter.js
+++ backend/routers/projectRouter.js
@@ -1,19 +1,24 @@
 import express from "express";
-import expressAsyncHandler from "express-async-handler"
+import expressAsyncHandler from "express-async-handler";
 import data from "../data.js";
-import Project from "../models/projectModel.js"
+import Project from "../models/projectModel.js";
 
 const projectRouter = express.Router();
 
-projectRouter.get("/seed", expressAsyncHandler(async (req, res) => {
-    const createdProjects = await Project.insertMany(data.projects);
-    res.send({ createdProjects });
-}))
+projectRouter.get(
+	"/seed",
+	expressAsyncHandler(async (req, res) => {
+		//const createdProjects = await Project.insertMany(data.projects);
+		//res.send({ createdProjects });
+	})
+);
 
-projectRouter.get("/", expressAsyncHandler(async (req, res) => {
-    const data = await Project.find({}).sort({ _id: 1 });
-    res.send(data);
-}))
-
+projectRouter.get(
+	"/",
+	expressAsyncHandler(async (req, res) => {
+		const data = await Project.find({}).sort({ _id: 1 });
+		res.send(data);
+	})
+);
 
 export default projectRouter;
